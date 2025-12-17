f729f6ee89c86e12f84d8846ca59427ab47fb95d
KakarotDB <mrigajpersonal@gmail.com>
2025-12-17 18:03:25 +0530

    Initial commit, added server and client files

---
+++ client.py
@@ -0,0 +1,77 @@
+import socket
+import sys
+import threading
+
+HOST = "127.0.0.1" #This can be changed as per required
+PORT = 65432
+
+def receive_messages(client_socket):
+    """
+    Runs in background thread
+    Constantly listens for messages from server and prints them
+    """
+
+    while True:
+        try:
+            message = client_socket.recv(1024).decode('utf-8')
+            if not message:
+                print(f"\n[DISCONNECTED] Server closed connection")
+                client_socket.close()
+                sys.exit()
+
+            print(f"[SERVER] {message}")
+            print(f"\nYou: ", end="", flush=True)
+        except OSError:
+            break
+        except KeyboardInterrupt:
+            print("[ERROR] Connection lost.")
+            break
+        except ConnectionResetError:
+            print("[CONNECTION RESET]")
+            break
+        except ConnectionAbortedError:
+            print("[CONNECTION ABORTED]")
+            break
+        except Exception as e:
+            print(f"[ERROR] Connection lost : {e}")
+            break
+
+def start_client():
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        client_socket.connect((HOST, PORT))
+    except ConnectionRefusedError:
+        print("[CONNECTION REFUSED]")
+        return
+
+    print("[CONNECTED] Connected to server!")
+
+    # Start the listener thread
+    receive_thread = threading.Thread(target=receive_messages, args=(client_socket,))
+    receive_thread.daemon = True
+    receive_thread.start()
+
+    while True:
+        try:
+            message = input("You: ")
+
+            if message.lower() == "bye":
+                client_socket.sendall(message.encode('utf-8'))
+                break
+
+            client_socket.sendall(message.encode('utf-8'))
+        except(KeyboardInterrupt, OSError, EOFError):
+            print("[EXIT] Exiting chat...")
+            try:
+                client_socket.sendall("bye".encode('utf-8')) #send bye message to inform server
+            except Exception as e:
+                print(f"[EXIT] Exiting chat with error {e}")
+            break
+
+    client_socket.close()
+
+if __name__ == "__main__":
+    try:
+        start_client()
+    except Exception as e:
+       print(f"\n[FORCE EXIT] Client Stopped with Exception {e}")
---
+++ server.py
@@ -0,0 +1,78 @@
+import socket
+import threading
+
+HOST = "127.0.0.1" #Change as per requirements (LAN wifi/Tunneling/etc)
+PORT = 65432
+clients = [] #Global list of clients
+
+def broadcast(message, source_connection=None):
+    """
+    Sends a message to all clients except the sender
+    """
+    for client in clients:
+        if client != source_connection:
+            try:
+                client.sendall(message)
+            except:
+                clients.remove(client)
+
+def handle_client(connection, address):
+    print(f"[NEW CONNECTION] {address} Connected")
+    connected = True
+
+    while connected:
+        try:
+            data = connection.recv(1024)
+            if not data:
+                break
+
+            message = data.decode('utf-8')
+
+            if  message.lower() == "bye":
+                connected = False
+            else:
+                final_msg = f"Client {address[1]}: {message}"
+                print(f"[BROADCAST] {final_msg}")
+                broadcast(final_msg.encode('utf-8'), connection)
+        except ConnectionResetError:
+            break
+
+    if connection in clients:
+        clients.remove(connection)
+
+    connection.close()
+
+    print(f"[CLOSED] Connection with {address} closed.")
+    print(f"[ACTIVE CONNECTIONS] {len(clients)}")
+
+
+def start_server():
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server.bind((HOST, PORT))
+    server.listen()
+    print(f"[LISTENING] Server is listening on {HOST}:{PORT}")
+    server.settimeout(1.0)
+    try:
+        while True:
+            try:
+                connection, address = server.accept()
+
+                clients.append(connection)
+                thread = threading.Thread(target=handle_client, args=(connection, address))
+                thread.daemon = True
+                thread.start()
+
+                print(f"[ACTIVE CONNECTIONS] {threading.active_count() - 1}")
+            except socket.timeout:
+                continue
+    except KeyboardInterrupt:
+        print("[STOPPING] Server is shutting down...")
+    except ConnectionResetError:
+        print("[CONNECTION RESET ERROR]")
+    finally:
+        server.close()
+        print("[CLOSED] Server socket closed.")
+
+if __name__ == "__main__":
+    start_server()
